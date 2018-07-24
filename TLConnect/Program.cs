6f06a2a53df54cc477e705ac84ab4bc2d3c6e405
Holger Prohl <holger.prohl@gmx.de>
2018-07-24 11:50:22 +0200

    Upload
    Project Init

---
+++ TLConnect/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace TLConnect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tlSender = "PCE-01";
+            string tlReceiver = "PCE";
+            string tlRequestId = "201807180900";
+            string tlGtin = "02768000000014";
+            string tlUser = "holger.prohl@mt.com";
+            string tlPassword = "##Hjk12gf$";
+            int tlAmount = 100;
+
+            Console.WriteLine("Tracelink Connect");
+            Console.WriteLine("\n\n");
+
+            TraceLink.objectKey tlObjectKey = new TraceLink.objectKey
+            {
+                Name = TraceLink.objectIdentifierType.GTIN,
+                Value = tlGtin
+            };
+
+            tlObjectKey.NameSpecified = true;
+
+            TraceLink.SNResponse tlSNResponse = new TraceLink.SNResponse();
+       
+            TraceLink.ReferenceDocuments referenceDocuments = new TraceLink.ReferenceDocuments();
+            TraceLink.snrequestClient tlClient = new TraceLink.snrequestClient();
+            tlClient.ClientCredentials.UserName.UserName = tlUser;
+            tlClient.ClientCredentials.UserName.Password = tlPassword;
+            
+
+            try
+            {
+                tlClient.Open();
+                Console.WriteLine("Client state: " + tlClient.State.ToString());
+                tlSNResponse = tlClient.serialNumbersRequest(tlSender, tlReceiver, 
+                    TraceLink.idType.GS1_SER, TraceLink.encodingType.SGTIN, 
+                    tlAmount, tlObjectKey, 
+                    tlRequestId, referenceDocuments);
+            }
+            catch (Exception error)
+            {
+
+                Console.WriteLine("Error: " + error.Message.ToString());
+            }
+            finally
+            {
+                Console.WriteLine(tlSNResponse.RandomizedNumberList);
+                Console.ReadLine();
+            }
+        }    
+    }
+}
---
+++ TLConnect/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("TLConnect")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TLConnect")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("986ca7a7-ce80-4614-b6bf-872e415de580")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
